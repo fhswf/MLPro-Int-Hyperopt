a3e908e28b837663ead9114caa91a5b070286566
Laxmikant <laxmikant.baheti.s@gmail.com>
2024-02-17 11:45:39 +0100

    updated release version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro_int_hyperopt',
-version='0.1.0',
+version='0.1.2',
 description='MLPro: Integration Hyperopt',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
