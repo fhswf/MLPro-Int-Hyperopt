7835ebd7fc33dbd56c136bc0edba6f3d903b5dae
Steve Yuwono <88610818+steveyuwono@users.noreply.github.com>
2025-08-03 20:36:52 +0200

    Update hyperopt.py

--- src/mlpro_int_hyperopt/wrappers/hyperopt.py
+++ src/mlpro_int_hyperopt/wrappers/hyperopt.py
@@ -38,6 +38,7 @@ from mlpro.bf.ml import *
 from mlpro.bf.math import *
 from mlpro.bf.data import *
 from mlpro.bf import *
+from mlpro.bf.various import *
 from mlpro.rl import *
 from mlpro.gt.dynamicgames import *
 import os
